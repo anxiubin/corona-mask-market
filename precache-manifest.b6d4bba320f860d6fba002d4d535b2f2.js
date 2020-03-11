6c2217ea1db7dbdeb333ae74c713047a565eb8b7
anxiubin <tnqls0120@naver.com>
2020-03-11 17:53:18 +0900

    Updates

---
+++ precache-manifest.b6d4bba320f860d6fba002d4d535b2f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f771c9cf6ae8df72d9cbf5eddc6e4b6",
+    "url": "/corona-mask-market/index.html"
+  },
+  {
+    "revision": "ca23f00a2e3b94012a2c",
+    "url": "/corona-mask-market/static/css/main.ec371559.chunk.css"
+  },
+  {
+    "revision": "2c23e8374368ac734d44",
+    "url": "/corona-mask-market/static/js/2.623452d2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/corona-mask-market/static/js/2.623452d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca23f00a2e3b94012a2c",
+    "url": "/corona-mask-market/static/js/main.6c3f6feb.chunk.js"
+  },
+  {
+    "revision": "ca03cf6ba118092bf361",
+    "url": "/corona-mask-market/static/js/runtime-main.86c6304b.js"
+  },
+  {
+    "revision": "b15b6fb2418134593d3bd42f0d628a50",
+    "url": "/corona-mask-market/static/media/home-img.b15b6fb2.png"
+  },
+  {
+    "revision": "b22eef93c5c51f595e0960a04dfc3f04",
+    "url": "/corona-mask-market/static/media/korea-map.b22eef93.png"
+  }
+]);
