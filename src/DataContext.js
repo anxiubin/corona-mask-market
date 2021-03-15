6c95f5840d335ceb60f81985a7eb3027ee86475a
anxiubin <tnqls0120@naver.com>
2021-03-15 19:56:39 +0900

    ENH: COVID19 데이터 API 변경 - 보건복지부 openAPI로 변경

--- src/DataContext.js
+++ src/DataContext.js
@@ -1,284 +1,294 @@
-import React, { createContext , useContext } from 'react';
-import axios from 'axios';
-import dotenv from 'dotenv';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import produce from 'immer';
+import React, { createContext, useContext } from "react"
+import axios from "axios"
+import dotenv from "dotenv"
+import { useState } from "react"
+import { useEffect } from "react"
+import produce from "immer"
 
-dotenv.config();
+dotenv.config()
 
-const COVID_APIKEY = process.env.REACT_APP_COVID19_API_KEY;
-
+const COVID_APIKEY = process.env.REACT_APP_COVID19_API_KEY
 const cities = [
-    {
-        id: 'seoul',
-        idKR: '서울',
-        idToast: 'KR-SU',
-        num: 0,
-        url: 'http://www.seoul.go.kr/coronaV/coronaStatus.do'
-    },
-    {
-        id: 'gyeonggi',
-        idKR: '경기',
-        idToast: 'KR-GG',
-        num: 0,
-        url: 'https://www.gg.go.kr/bbs/boardView.do?bsIdx=464&bIdx=2296956&menuId=1535'
-    },
-    {
-        id: 'incheon',
-        idKR: '인천',
-        idToast: 'KR-IC',
-        num: 0,
-        url: 'https://www.incheon.go.kr/'
-    },
-    {
-        id: 'gangwon',
-        idKR: '강원',
-        idToast: 'KR-GW',
-        num: 0,
-        url: 'http://www.provin.gangwon.kr/'
-    },
-    {
-        id: 'chungnam',
-        idKR: '충남',
-        idToast: 'KR-SC',
-        num: 0,
-        url: 'http://www.chungnam.go.kr/coronaStatus.do'
-    },
-    {
-        id: 'chungbuk',
-        idKR: '충북',
-        idToast: 'KR-NC',
-        num: 0,
-        url: 'http://www.chungbuk.go.kr/'
-    },
-    {
-        id: 'sejong',
-        idKR: '세종',
-        idToast: 'KR-SE',
-        num: 0,
-        url: 'https://www.sejong.go.kr/life/sub05_0704.do'
-    },
-    {
-        id: 'daejeon',
-        idKR: '대전',
-        idToast: 'KR-DJ',
-        num: 0,
-        url: 'https://www.daejeon.go.kr/corona19/index.do'
-    },
-    {
-        id: 'jeonbuk',
-        idKR: '전북',
-        idToast: 'KR-NJ',
-        num: 0,
-        url: 'http://www.jeonbuk.go.kr/'
-    },
-    {
-        id: 'jeonnam',
-        idKR: '전남',
-        idToast: 'KR-SJ',
-        num: 0,
-        url: 'https://www.jeonnam.go.kr/coronaMainPage.do'
-    },
-    {
-        id: 'gwangju',
-        idKR: '광주',
-        idToast: 'KR-GJ',
-        num: 0,
-        url: 'https://www.gwangju.go.kr/'
-    },
-    {
-        id: 'gyeongbuk',
-        idKR: '경북',
-        idToast: 'KR-NG',
-        num: 0,
-        url: 'http://www.gb.go.kr/Main/index.html'
-    },
-    {
-        id: 'gyeongnam',
-        idKR: '경남',
-        idToast: 'KR-SG',
-        num: 0,
-        url: 'http://www.gyeongnam.go.kr/corona.html'
-    },
-    {
-        id: 'daegu',
-        idKR: '대구',
-        idToast: 'KR-DG',
-        num: 0,
-        url: 'http://www.daegu.go.kr/'
-    },
-    {
-        id: 'ulsan',
-        idKR: '울산',
-        idToast: 'KR-US',
-        num: 0,
-        url: 'http://www.ulsan.go.kr/corona.jsp'
-    },
-    {
-        id: 'busan',
-        idKR: '부산',
-        idToast: 'KR-BS',
-        num: 0,
-        url: 'http://www.busan.go.kr/corona19/index'
-    },
-    {
-        id: 'jeju',
-        idKR: '제주',
-        idToast: 'KR-JJ',
-        num: 0,
-        url: 'https://jeju.go.kr/covid19.jsp'
-    },
-    {
-        id: 'quarantine',
-        idKR: '검역',
-        num: 0,
-        url: 'http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=13&ncvContSeq=&contSeq=&board_id=&gubun='
-    },
-];
+	{
+		id: "Seoul",
+		idKR: "서울",
+		idToast: "KR-SU",
+		num: 0,
+		todayNum: 0,
+		url: "http://www.seoul.go.kr/coronaV/coronaStatus.do",
+	},
+	{
+		id: "Gyeonggi-do",
+		idKR: "경기",
+		idToast: "KR-GG",
+		num: 0,
+		todayNum: 0,
+		url:
+			"https://www.gg.go.kr/bbs/boardView.do?bsIdx=464&bIdx=2296956&menuId=1535",
+	},
+	{
+		id: "Incheon",
+		idKR: "인천",
+		idToast: "KR-IC",
+		num: 0,
+		todayNum: 0,
+		url: "https://www.incheon.go.kr/",
+	},
+	{
+		id: "Gangwon-do",
+		idKR: "강원",
+		idToast: "KR-GW",
+		num: 0,
+		todayNum: 0,
+		url: "http://www.provin.gangwon.kr/",
+	},
+	{
+		id: "Chungcheongnam-do",
+		idKR: "충남",
+		idToast: "KR-SC",
+		num: 0,
+		todayNum: 0,
+		url: "http://www.chungnam.go.kr/coronaStatus.do",
+	},
+	{
+		id: "Chungcheongbuk-do",
+		idKR: "충북",
+		idToast: "KR-NC",
+		num: 0,
+		todayNum: 0,
+		url: "http://www.chungbuk.go.kr/",
+	},
+	{
+		id: "Sejong",
+		idKR: "세종",
+		idToast: "KR-SE",
+		num: 0,
+		todayNum: 0,
+		url: "https://www.sejong.go.kr/life/sub05_0704.do",
+	},
+	{
+		id: "Daejeon",
+		idKR: "대전",
+		idToast: "KR-DJ",
+		num: 0,
+		todayNum: 0,
+		url: "https://www.daejeon.go.kr/corona19/index.do",
+	},
+	{
+		id: "Jeollabuk-do",
+		idKR: "전북",
+		idToast: "KR-NJ",
+		num: 0,
+		todayNum: 0,
+		url: "http://www.jeonbuk.go.kr/",
+	},
+	{
+		id: "Jeollanam-do",
+		idKR: "전남",
+		idToast: "KR-SJ",
+		num: 0,
+		todayNum: 0,
+		url: "https://www.jeonnam.go.kr/coronaMainPage.do",
+	},
+	{
+		id: "Gwangju",
+		idKR: "광주",
+		idToast: "KR-GJ",
+		num: 0,
+		todayNum: 0,
+		url: "https://www.gwangju.go.kr/",
+	},
+	{
+		id: "Gyeongsangbuk-do",
+		idKR: "경북",
+		idToast: "KR-NG",
+		num: 0,
+		todayNum: 0,
+		url: "http://www.gb.go.kr/Main/index.html",
+	},
+	{
+		id: "Gyeongsangnam-do",
+		idKR: "경남",
+		idToast: "KR-SG",
+		num: 0,
+		todayNum: 0,
+		url: "http://www.gyeongnam.go.kr/corona.html",
+	},
+	{
+		id: "Daegu",
+		idKR: "대구",
+		idToast: "KR-DG",
+		num: 0,
+		todayNum: 0,
+		url: "http://www.daegu.go.kr/",
+	},
+	{
+		id: "Ulsan",
+		idKR: "울산",
+		idToast: "KR-US",
+		num: 0,
+		todayNum: 0,
+		url: "http://www.ulsan.go.kr/corona.jsp",
+	},
+	{
+		id: "Busan",
+		idKR: "부산",
+		idToast: "KR-BS",
+		num: 0,
+		todayNum: 0,
+		url: "http://www.busan.go.kr/corona19/index",
+	},
+	{
+		id: "Jeju",
+		idKR: "제주",
+		idToast: "KR-JJ",
+		num: 0,
+		todayNum: 0,
+		url: "https://jeju.go.kr/covid19.jsp",
+	},
+	{
+		id: "Lazaretto",
+		idKR: "검역",
+		num: 0,
+		todayNum: 0,
+		url:
+			"http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=13&ncvContSeq=&contSeq=&board_id=&gubun=",
+	},
+]
 
-const getCovid19Data = (city) => {
-    return axios({
-        method: "GET",
-        url: `https://cors-anywhere.herokuapp.com/https://api.dropper.tech/covid19/status/korea?locale=${city}`,
-        //CORS 이슈 프록시 서버를 통해 해결
-        headers: {
-            'APIKey': COVID_APIKEY,
-        },
-    });    
-};
+const DataStateContext = createContext(null)
+const LocalDataStateContext = createContext(null)
 
-const DataStateContext = createContext(null);
-const LocalDataStateContext = createContext(null);
+//context API provider사용할 수 있는 컴포넌트
+export function DataProvider({ children }) {
+	const [state, setState] = useState({
+		confirmed: undefined,
+		dead: undefined,
+		deisolated: undefined,
+		updateTime: undefined,
+	})
 
-//context API provider사용할 수 있는 컴포넌트 
-export function DataProvider({children}){
+	const [local, setLocal] = useState(cities)
 
-    const [state, setState] = useState({
-        certified: undefined,
-        dead: undefined,
-        deisolated: undefined,
-        updateTime: undefined,
-    });
+	//전국 확진자
+	// useEffect(() => {
+	// 	if (state.confirmed === undefined) {
+	// 		getCovid19Data("synthesize")
+	// 			.then((response) => {
+	// 				if (response.data.status.code === 200) {
+	// 					const updateTime = new Date(
+	// 						response.data.data[0].announced_timestamp * 1000
+	// 					)
+	// 					const dateString = updateTime.toLocaleString("ko-KR", {
+	// 						year: "numeric",
+	// 						month: "long",
+	// 						day: "numeric",
+	// 					})
 
-    const [local, setLocal] = useState(cities);
+	// 					console.log("result", response.data)
+	// 					console.log("time", dateString)
 
-    //전국 확진자
-    useEffect(() => {
-        if(state.certified === undefined) {
-            getCovid19Data('synthesize').then(response => {
-                if(response.data.status.code === 200) {
-                    const updateTime = new Date(response.data.data[0].announced_timestamp * 1000);
-                    const dateString = updateTime.toLocaleString('ko-KR', {
-                        year: 'numeric',
-                        month: 'long',
-                        day: 'numeric'
-                    });
+	// 					setState({
+	// 						...state,
+	// 						confirmed: response.data.data[0].confirmed,
+	// 						dead: response.data.data[0].dead,
+	// 						deisolated: response.data.data[0].deisolated,
+	// 						updateTime: dateString,
+	// 					})
+	// 				} else {
+	// 					console.log("There is an error")
+	// 				}
+	// 			})
+	// 			.catch((e) => console.error(e))
+	// 	}
+	// }, [])
 
-                    console.log('result', response.data);
-                    console.log('time', dateString );
+	// covid19 api 호출
+	useEffect(() => {
+		var xhr = new XMLHttpRequest()
+		var url = "/getCovid19SidoInfStateJson"
+		var queryParams =
+			"?" + encodeURIComponent("ServiceKey") + "=" + COVID_APIKEY
+		queryParams +=
+			"&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1")
+		queryParams +=
+			"&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("10")
+		queryParams +=
+			"&" +
+			encodeURIComponent("startCreateDt") +
+			"=" +
+			encodeURIComponent("20210315")
+		queryParams +=
+			"&" +
+			encodeURIComponent("endCreateDt") +
+			"=" +
+			encodeURIComponent("20210315")
+		xhr.open("GET", url + queryParams)
+		xhr.onreadystatechange = function () {
+			if (this.readyState === 4) {
+				var xml = this.responseXML
+				var regionsData = xml.getElementsByTagName("gubunEn")
+				var confirmedData = xml.getElementsByTagName("defCnt")
+				var deathData = xml.getElementsByTagName("deathCnt")
+				var deisolatedData = xml.getElementsByTagName("isolClearCnt")
+				var updateTimeData = xml.getElementsByTagName("createDt")
 
-                    setState({
-                        ...state,
-                        certified: response.data.data[0].certified,
-                        dead: response.data.data[0].dead,
-                        deisolated: response.data.data[0].deisolated,
-                        updateTime: dateString,
-                    });
-                } else {
-                    console.log('There is an error');
-                }
-            }).catch(e => console.error(e));
-        }
-    },[]);
+				if (local[0].num === 0) {
+					for (var i = 0; i < regionsData.length; i++) {
+						var region = regionsData[i].childNodes[0].nodeValue
+						var confirmed = confirmedData[i].childNodes[0].nodeValue
+						var dead = deathData[i].childNodes[0].nodeValue
+						var deisolated = deisolatedData[i].childNodes[0].nodeValue
+						var updateTime = updateTimeData[i].childNodes[0].nodeValue
 
-    useEffect(() => {
-        //마운트 된 이후 6시간 단위 업데이트
-        const dataInterval = setInterval(() => {
-            getCovid19Data().then(response => {
-                if(response.data.status.code === 200) {
-                    console.log('result2', response.data.data[0]);
-                    setState({
-                        ...state,
-                        certified: response.data.data[0].certified,
-                        dead: response.data.data[0].dead,
-                        deisolated: response.data.data[0].deisolated,
-                    });
-                } else {
-                    console.log('There is an error');
-                }
-            }).catch(e => console.error(e));
-        }, 21600000);
+						// eslint-disable-next-line no-loop-func
+						local.forEach((city, index) => {
+							if (city.id === region)
+								setLocal(
+									produce((draft) => {
+										draft[index].num = confirmed
+									})
+								)
+						})
+					}
+					if (region === "Total") {
+						setState({
+							...state,
+							confirmed,
+							dead,
+							deisolated,
+							updateTime,
+						})
+					}
+				}
+			}
+		}
 
-        return () => {
-            clearInterval(dataInterval);
-            console.log('unmount');
-        }
-    },[]);
+		xhr.send("")
+	}, [])
 
-
-    //도시 별 확진자
-    useEffect(() => {
-        if(local[0].num === 0) {
-            local.forEach((city, i) => {
-                getCovid19Data(city.id).then(response => {    
-                    if(response.data.status.code === 200) {
-                        setLocal(
-                            produce(draft => {
-                                draft[i].num = response.data.data[0].certified;
-                            })
-                        ); //Immer 사용
-                    } else {
-                        console.log('There is an error');
-                    }
-                }).catch(e => console.error(e));
-            })               
-        }
-    },[]);
-
-    useEffect(() => {
-        //마운트 된 이후 6시간 단위 업데이트
-        const dataInterval = setInterval(() => {
-            local.forEach((city, i) => {
-                getCovid19Data(city.id).then(response => {    
-                    if(response.data.status.code === 200) {
-                        setLocal(
-                            produce(draft => {
-                                draft[i].num = response.data.data[0].certified;
-                            })
-                        ); //Immer 사용
-                    } else {
-                        console.log('There is an error');
-                    }
-                }).catch(e => console.error(e));
-            }) 
-        }, 21600000);
-            
-        return () => clearInterval(dataInterval);
-    },[]);
-
-    return (
-        <DataStateContext.Provider value={state}>
-            <LocalDataStateContext.Provider value={local}>
-                {children}
-            </LocalDataStateContext.Provider>
-        </DataStateContext.Provider>
-    );
+	return (
+		<DataStateContext.Provider value={state}>
+			<LocalDataStateContext.Provider value={local}>
+				{children}
+			</LocalDataStateContext.Provider>
+		</DataStateContext.Provider>
+	)
 }
 
 //custom HOOK
 export function useDataState() {
-    const context = useContext(DataStateContext);
-    if(!context) {
-      throw new Error('Cannot find DataStateContext')
-    }
-      return useContext(DataStateContext);
-  }
+	const context = useContext(DataStateContext)
+	if (!context) {
+		throw new Error("Cannot find DataStateContext")
+	}
+	return useContext(DataStateContext)
+}
 
 export function useLocalDataState() {
-    const context = useContext(LocalDataStateContext);
-    if(!context) {
-      throw new Error('Cannot find LocalDataStateContext')
-    }
-      return useContext(LocalDataStateContext);
-  }
+	const context = useContext(LocalDataStateContext)
+	if (!context) {
+		throw new Error("Cannot find LocalDataStateContext")
+	}
+	return useContext(LocalDataStateContext)
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,112 +1,110 @@
-import React, {useState, useEffect} from 'react';
-import LocalInfections from './LocalInfections';
-import KoreaMap from './KoreaMap';
-import { useDataState } from '../DataContext';
-
+import React, { useState, useEffect } from "react"
+import LocalInfections from "./LocalInfections"
+import KoreaMap from "./KoreaMap"
+import { useDataState } from "../DataContext"
 
 function Home() {
+	const data = useDataState()
 
-    const data = useDataState();
- 
-    const [trueNum, setTrueNum] = useState(0);
-    const [deadNum, setDeadNum] = useState(0);
-    const [falseNum, setFalseNum] = useState(0);
+	const [trueNum, setTrueNum] = useState(0)
+	const [deadNum, setDeadNum] = useState(0)
+	const [falseNum, setFalseNum] = useState(0)
 
-    useEffect(() => {
-        let diff;
-        const counter = () => {
-            diff = data.certified - trueNum;
+	//count up for infectionTrue
+	useEffect(() => {
+		let diff
+		const counter = () => {
+			diff = data.confirmed - trueNum
 
-        if(diff > 0) {
-            setTrueNum(trueNum => trueNum + Math.ceil(diff / 5));
-            }
-        }
+			if (diff > 0) {
+				setTrueNum((trueNum) => trueNum + Math.ceil(diff / 5))
+			}
+		}
 
-        let runCounter;
+		let runCounter
 
-        if(trueNum < data.certified) {
-            runCounter = setTimeout(() => {
-                counter();
-            }, 10);
-        }
-        return () => clearTimeout(runCounter);
-    }, [trueNum, data]);
+		if (trueNum < data.confirmed) {
+			runCounter = setTimeout(() => {
+				counter()
+			}, 10)
+		}
+		return () => clearTimeout(runCounter)
+	}, [trueNum, data])
 
-    //count up for infectionDead
-    useEffect(() => {
-        const interval = setInterval(() => {
-            if(deadNum < data.dead) {
-                setDeadNum(num => num + 1);
-            }
-        }, 1);
-        return () => clearInterval(interval);
-    }, [deadNum, data]);
+	//count up for infectionDead
+	useEffect(() => {
+		let diff
+		const counter = () => {
+			diff = data.dead - deadNum
 
-    //count up for infectionFalse
-    useEffect(() => {
-        let diff;
-        const counter = () => {
-            diff = data.deisolated - falseNum;
+			if (diff > 0) {
+				setDeadNum((num) => num + Math.ceil(diff / 5))
+			}
+		}
 
-        if(diff > 0) {
-            setFalseNum(falseNum => falseNum + Math.ceil(diff / 5));
-            }
-        }
+		let runCounter
 
-        let runCounter;
+		if (deadNum < data.dead) {
+			runCounter = setTimeout(() => {
+				counter()
+			}, 10)
+		}
+		return () => clearTimeout(runCounter)
+	}, [deadNum, data])
 
-        if(falseNum < data.deisolated) {
-            runCounter = setTimeout(() => {
-                counter();
-            }, 10);
-        }
-        return () => clearTimeout(runCounter);
-    }, [falseNum, data]);
+	//count up for infectionFalse
+	useEffect(() => {
+		let diff
+		const counter = () => {
+			diff = data.deisolated - falseNum
 
+			if (diff > 0) {
+				setFalseNum((falseNum) => falseNum + Math.ceil(diff / 5))
+			}
+		}
 
-    return (
-        <div>
-            <section>
-            <div className="home-img-section">
-                <h1>
-                    #코로나 마스크 마켓
-                </h1>
-            </div>
-            </section>
-            <section className="infection-wrap">
-                <div>
-                    <div className="updatetime">
-                        {data.updateTime} 기준
-                    </div>
-                    <div className="infection-data">
-                        <div className="infection-true">
-                            확진
-                            <div className="infection-true-num">
-                                {trueNum}
-                            </div>
-                        </div>
-                        <div className="infection-dead">
-                            사망
-                            <div className="infection-dead-num">
-                                {deadNum}
-                            </div>
-                        </div>
-                        <div className="infection-false">
-                            격리해제
-                            <div className="infection-false-num">
-                                {falseNum}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="map">
-                    <KoreaMap />
-                    <LocalInfections />
-                </div>
-            </section>
+		let runCounter
 
-        </div>
-    );
+		if (falseNum < data.deisolated) {
+			runCounter = setTimeout(() => {
+				counter()
+			}, 10)
+		}
+		return () => clearTimeout(runCounter)
+	}, [falseNum, data])
+
+	return (
+		<div>
+			<section>
+				<div className="home-img-section">
+					<h1>#코로나 마스크 마켓</h1>
+				</div>
+			</section>
+			<section className="infection-wrap">
+				<div>
+					<div className="updatetime">{data.updateTime} 기준</div>
+					<div className="infection-data">
+						<div className="infection-true">
+							확진
+							<div className="infection-true-num">{trueNum}</div>
+						</div>
+						<div className="infection-dead">
+							사망
+							<div className="infection-dead-num">{deadNum}</div>
+						</div>
+						<div className="infection-false">
+							격리해제
+							<div className="infection-false-num">{falseNum}</div>
+						</div>
+					</div>
+				</div>
+				<div className="map">
+					<KoreaMap />
+					<LocalInfections />
+				</div>
+			</section>
+		</div>
+	)
 }
 
-export default Home;
+export default Home
