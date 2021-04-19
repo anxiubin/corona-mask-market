c15c28d5b669b1345401a60504c9889de9de7e6e
anxiubin <tnqls0120@naver.com>
2021-04-19 17:35:18 +0900

    fix setupProxy.js

--- client/setupProxy.js
+++ client/setupProxy.js
@@ -1,10 +1,11 @@
 const { createProxyMiddleware } = require("http-proxy-middleware")
+const port = process.env.PORT || "http://localhost:5000"
 
 module.exports = function (app) {
 	app.use(
 		"/api/covid",
 		createProxyMiddleware({
-			target: "http://localhost:5000",
+			target: port,
 			changeOrigin: true,
 		})
 	)
